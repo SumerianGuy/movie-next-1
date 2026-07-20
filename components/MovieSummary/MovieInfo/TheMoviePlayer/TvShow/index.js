84652ae302d76db6f62ea3df63f96f3faccad2bc
sumerian <com4171@yahoo.com>
2026-07-20 02:11:22 +0100

    WW for movies only

--- components/MovieSummary/MovieInfo/TheMoviePlayer/TvShow/index.js
+++ components/MovieSummary/MovieInfo/TheMoviePlayer/TvShow/index.js
@@ -137,26 +137,26 @@ const MoviePlayer = ({ videoUrl, season = 1, episode = 1 }) => {
     }, []);
 
 
-  useEffect(() => {
-    const handleFirstClick = () => {
-      let trafficSource = "worldwide_other";
-      const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+  // useEffect(() => {
+  //   const handleFirstClick = () => {
+  //     let trafficSource = "worldwide_other";
+  //     const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
-      if (timeZone.includes("America") || timeZone.includes("US")) {
-        trafficSource = "USA";
-      } else if (timeZone.includes("Calcutta") || timeZone.includes("Asia/Kolkata")) {
-        trafficSource = "India";
-      }
+  //     if (timeZone.includes("America") || timeZone.includes("US")) {
+  //       trafficSource = "USA";
+  //     } else if (timeZone.includes("Calcutta") || timeZone.includes("Asia/Kolkata")) {
+  //       trafficSource = "India";
+  //     }
 
-      // Explicitly tags the traffic source as tv_player
-      const smartLinkUrl = `https://hippogrypos.com{trafficSource}`;
-      window.open(smartLinkUrl, '_blank');
-      document.removeEventListener('click', handleFirstClick);
-    };
+  //     // Explicitly tags the traffic source as tv_player
+  //     const smartLinkUrl = `https://hippogrypos.com{trafficSource}`;
+  //     window.open(smartLinkUrl, '_blank');
+  //     document.removeEventListener('click', handleFirstClick);
+  //   };
 
-    document.addEventListener('click', handleFirstClick);
-    return () => document.removeEventListener('click', handleFirstClick);
-  }, []);
+  //   document.addEventListener('click', handleFirstClick);
+  //   return () => document.removeEventListener('click', handleFirstClick);
+  // }, []);
 
 
 
